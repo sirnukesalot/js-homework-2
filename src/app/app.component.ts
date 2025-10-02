b369e202cc3d03e615b64514de431671e0adb431
sirnukesalot <123380040+sirnukesalot@users.noreply.github.com>
2025-10-02 20:20:14 +0500

    new version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'about-us';
+  likes = 0;
+  isWorking = true;
+  email = '';
+  title = 'About Our Team';
+  description = 'We provide different applications with consistent support '
+  showMessage = false;
+  emailSent = false;
+
+  imageURL = signal("https://t3.ftcdn.net/jpg/03/08/59/78/360_F_308597849_Sv10jeDceT36mhjJe9N64Left5QtXoIq.jpg")
+
+  like(){
+    this.likes++
+  }
+
+  toggleMessage(){
+    this.showMessage = !this.showMessage;
+  }
+
+  sendEmail(){
+    if(this.email.trim()){
+      this.emailSent = true;
+    }
+  }
 }
